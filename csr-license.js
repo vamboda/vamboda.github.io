542af5f86ffbcac609d114d5dcd4538bdd1c999a
vamboda <vamshidharreddyb@gmail.com>
2016-11-15 13:30:24 +0530

    d

--- csr-license.js
+++ csr-license.js
@@ -3,23 +3,24 @@ console.log(window.location.hostname);
 var iframe = document.createElement('iframe');
 iframe.src = "https://corporatematching.org/index_clients.html";
 iframe.title="Search your company:"
+myPara.setAttribute("id", "corporate-matching-iframe");
 iframe.onload = function() {
         var domain = '*';
-        var iframe = '';
-        var iframeElements = document.getElementsByTagName('iframe');
-        for (var j = 0; j < iframeElements.length; j++) {
-        var elem=iframeElements[j];
-            for (var i = 0; i < elem.attributes.length; i++) {
-                var attrib = elem.attributes[i];
-                if (attrib.specified && attrib.name == 'src') {
-                    if (attrib.value.includes('corporatematching.org')) {
-                        console.log(attrib.name + " = " + attrib.value);
-                        iframe = elem;
-                    }
-                }
-            }
-        }
-        
+        // var iframe = '';
+//         var iframeElements = document.getElementsByTagName('iframe');
+//         for (var j = 0; j < iframeElements.length; j++) {
+//         var elem=iframeElements[j];
+//             for (var i = 0; i < elem.attributes.length; i++) {
+//                 var attrib = elem.attributes[i];
+//                 if (attrib.specified && attrib.name == 'src') {
+//                     if (attrib.value.includes('corporatematching.org')) {
+//                         console.log(attrib.name + " = " + attrib.value);
+//                         iframe = elem;
+//                     }
+//                 }
+//             }
+//         }
+        var iframe = document.getElementById('corporate-matching-iframe').contentWindow;
         var message = "valuesplusfoundation.org";
 
         console.log('[SOURCE]blog.local:  sending message:  ' + message);
