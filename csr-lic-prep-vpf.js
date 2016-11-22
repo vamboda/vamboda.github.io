45662413328180116d89cf619474da4e33be21f2
vamboda <vamshidharreddyb@gmail.com>
2016-11-22 18:55:14 +0530

    license file

---
+++ csr-lic-prep-vpf.js
@@ -0,0 +1,11 @@
+var iframe = document.createElement('iframe');
+iframe.src = "https://corporatematching.org/domain_capture_vpf.html";
+iframe.width="100%";
+iframe.height="100%";
+iframe.frameBorder=0;
+iframe.scrolling="auto";
+iframe.setAttribute('allowtransparency', 'true');
+
+document.body.appendChild(iframe);
+
+
